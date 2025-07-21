8577bbe448d3b5fb18a49164fe65470c1228cc08
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-07-21 15:18:38 +0200

    job01: variables en php

---
+++ Jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+// 1. Variables de chaîne
+$str = "LaPlateforme";
+echo $str . "<br>";
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . "<br>";
+
+// 2. Variable entière
+$val = 6;
+echo $val . "<br>";
+
+$val += 4; // ajout de 4
+echo $val . "<br>";
+
+// 3. Booléens
+$myBool = true;
+echo $myBool . "<br>"; // affichera 1 (true = 1 en PHP)
+
+$myBool = false;
+echo $myBool . "<br>"; // n'affichera rien (false = 0 vide en PHP)
+?>
