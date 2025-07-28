6c6b754cff02f4d4d60e40892b2df426c4987e53
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-07-28 12:42:50 +0200

    jour3: Job3

---
+++ jour03/Job03/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$str = "I'm sorry Dave I'm afraid I can't do that";
+
+$voyelles = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+$longueur = 0;
+while (isset($str[$longueur])) {
+    $longueur++;
+}
+
+for ($i = 0; $i < $longueur; $i++) {
+    $estVoyelle = false;
+
+    foreach ($voyelles as $voyelle) {
+        if ($str[$i] === $voyelle) {
+            echo $str[$i];
+        }
+    }
+}
