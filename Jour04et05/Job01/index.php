1389626959ded83e1ae687c5df55f50b1a418c18
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-07-29 11:44:30 +0200

    Job1 de 4eme jour

---
+++ Jour04et05/Job01/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire GET</title>
+</head>
+
+<body>
+
+    <h1>Formulaire GET</h1>
+
+    <form action="" method="get">
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom"><br><br>
+
+        <label for="age">Âge :</label>
+        <input type="text" name="age" id="age"><br><br>
+
+        <label for="ville">Ville :</label>
+        <input type="text" name="ville" id="ville"><br><br>
+
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <hr>
+
+    <?php
+    if (!empty($_GET)) {
+        $nombreArguments = count($_GET);
+        echo "<p>Le nombre d'arguments GET envoyés est : <strong>$nombreArguments</strong></p>";
+        echo "<ul>";
+        foreach ($_GET as $cle => $valeur) {
+            echo "<li><strong>$cle</strong> : $valeur</li>";
+        }
+        echo "</ul>";
+    }
+    ?>
+
+</body>
+
+</html>
