f3bcb91c7bcdabea46abdcc30fd3833da1132cc7
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-08-05 15:02:06 +0200

    job03 du dernier jour

---
+++ Jour10/Job03/index.php
@@ -0,0 +1,29 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'jour09');
+
+if ($mysqli->connect_error) {
+    die('Erreur de connexion : ' . $mysqli->connect_error);
+}
+
+$result = $mysqli->query("SELECT prenom, nom, naissance FROM etudiants WHERE sexe = 'Femme'");
+
+if ($result && $result->num_rows > 0) {
+    echo '<table border="1">';
+    echo '<thead><tr>';
+    while ($field = $result->fetch_field()) {
+        echo '<th>' . $field->name . '</th>';
+    }
+    echo '</tr></thead><tbody>';
+    while ($rows = $result->fetch_assoc()) {
+        echo '<tr>';
+        foreach ($rows as $value) {
+            echo '<td>' . $value . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</tbody></table>';
+} else {
+    echo "aucun resultat n'est retourné";
+}
+
+$mysqli->close();
